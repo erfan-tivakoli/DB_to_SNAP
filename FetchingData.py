d8749b8d8b697980dd63793efb65b156f6f4d5ae
Erfan Tavakoli <erfan.tavakoli71@gmail.com>
2015-09-15 17:45:59 +0200

    Simple test on TNEANet Creation

--- FetchingData.py
+++ FetchingData.py
@@ -20,20 +20,37 @@ def add_edge(Graph, src_id, dst_id):
 
 Twitter = snap.TNEANet.New()
 """
-:type Twitter: snap.PNEANet
+:type Twitter: snap.TNEANet
 """
 
 conn = DbConnection()
 cur = conn.get_cursor()
-query = """select ida,idb from links limit 20"""
+query = """select max(ida) from links"""
+cur.execute(query)
+max_ida = cur.fetchone()
+query = """select max(idb) from links"""
+cur.execute(query)
+max_idb = cur.fetchone()
+total_users = max(max_ida, max_idb)
+print("total user is %d and max_ida is %d and max_idb is %d",(total_users, max_ida, max_idb))
+
+
+# for i in range(1,1000):
+query = """select ida,idb from links where ida<10000"""
 cur.execute(query)
 
-
+counter = 0
 for [ida, idb] in cur:
     add_edge(Twitter, ida, idb)
+    counter += 1
+    print('\r counter is %d' %counter)
 
-for NI in Twitter.Nodes():
-    print "node: %d, out-degree %d, in-degree %d" % ( NI.GetId(), NI.GetOutDeg(), NI.GetInDeg())
+FOut = snap.TFOut("test.graph")
+Twitter.Save(FOut)
+FOut.Flush()
 
-for EI in Twitter.Edges():
-    print "edge (%d, %d)" % (EI.GetSrcNId(), EI.GetDstNId())
+# for NI in Twitter.Nodes():
+#     print "node: %d, out-degree %d, in-degree %d" % ( NI.GetId(), NI.GetOutDeg(), NI.GetInDeg())
+#
+# for EI in Twitter.Edges():
+#     print "edge (%d, %d)" % (EI.GetSrcNId(), EI.GetDstNId())
