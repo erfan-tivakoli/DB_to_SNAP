13e2c9e73635a0ea44053a2941622ad79b0711b4
Erfan Tavakoli <erfan.tavakoli71@gmail.com>
2015-09-15 16:02:49 +0200

    Simple test on TNEANet Creation

--- FetchingData.py
+++ FetchingData.py
@@ -1,12 +1,39 @@
 from db_connector import DbConnection
-# import snap
+import snap
+
+
+
+def add_edge(Graph, src_id, dst_id):
+    """
+    :type Graph : snap.PNEANet
+    :param Graph:
+    :param src_id:
+    :param dst_id:
+    :return:
+    """
+    if not Graph.IsNode(src_id):
+        Graph.AddNode(src_id)
+    if not Graph.IsNode(dst_id):
+        Graph.AddNode(dst_id)
+    Graph.AddEdge(src_id, dst_id)
+
+
+Twitter = snap.TNEANet.New()
+"""
+:type Twitter: snap.PNEANet
+"""
 
 conn = DbConnection()
-
 cur = conn.get_cursor()
 query = """select ida,idb from links limit 20"""
 cur.execute(query)
 
-# Twitter = snap.TNEANet.New()
+
 for [ida, idb] in cur:
-    print ida
+    add_edge(Twitter, ida, idb)
+
+for NI in Twitter.Nodes():
+    print "node: %d, out-degree %d, in-degree %d" % ( NI.GetId(), NI.GetOutDeg(), NI.GetInDeg())
+
+for EI in Twitter.Edges():
+    print "edge (%d, %d)" % (EI.GetSrcNId(), EI.GetDstNId())
