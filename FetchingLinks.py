92b86e2d1218ae477dcde25814a7ca117e8ae07f
Erfan Tavakoli <erfan.tavakoli71@gmail.com>
2015-09-17 22:34:07 +0200

    exception handling added

--- FetchingLinks.py
+++ FetchingLinks.py
@@ -20,9 +20,10 @@ def add_edge(graph, src_id, dst_id):
         if not graph.IsEdge(src_id, dst_id):
             graph.AddEdge(src_id, dst_id)
     except RuntimeError, e:
-        sys.stderr.write('problem in adding %d -> %d' % (src_id, dst_id))
-        sys.stderr.write(str(e))
-
+        # sys.stderr.write('problem in adding %d -> %d' % (src_id, dst_id))
+        # sys.stderr.write(str(e))
+        pass
+    
 conn = DbConnection()
 cur = conn.get_cursor()
 query = """select max(ida) from links"""
@@ -58,7 +59,7 @@ while i < total_users:
         add_edge(Twitter, ida, idb)
     print('edges added to graph')
 
-    if (i % 10000000 is 0) or i >= total_users:
+    if (i % 4000000 is 0) or i >= total_users:
         print('saving the graph')
         FOut = snap.TFOut("test.graph")
         Twitter.Save(FOut)
