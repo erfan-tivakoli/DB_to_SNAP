e8ccc00b4ae735394b6932eaf234620edc181943
Erfan Tavakoli <erfan.tavakoli71@gmail.com>
2015-09-18 00:36:36 +0200

    exception handling added

--- FetchingTweetTimes.py
+++ FetchingTweetTimes.py
@@ -3,13 +3,13 @@ import threading
 import Queue
 from db_connector import DbConnection
 
-FIn = snap.TFIn("test.graph")
-Twitter = snap.TNEANet.Load(FIn)
-"""
-:type Twitter:snap.TNEANet
-"""
-print("the graph was loaded")
-Twitter.AddStrAttrN("TweetsTime")
+# FIn = snap.TFIn("test.graph")
+# Twitter = snap.TNEANet.Load(FIn)
+# """
+# :type Twitter:snap.TNEANet
+# """
+# print("the graph was loaded")
+# Twitter.AddStrAttrN("TweetsTime")
 
 
 def extract_tweets(thread_id, cur):
@@ -21,24 +21,27 @@ def extract_tweets(thread_id, cur):
     cur.execute(query, (start_point_id, start_point_id + chunk_size - 1))
     print('thread %d has fetched data from %d to %d' % (thread_id, start_point_id, start_point_id + chunk_size -1))
     cur.fetchall()
+    print("what the hell is going wrong*******")
     for [userid, tweettime] in cur:
-        threadLock.acquire()
-        if Twitter.IsNode(userid):
-            node_tweet_times = Twitter.GetStrAttrDatN(userid, "TweetsTime")
-            Twitter.AddStrAttrDatN(userid, node_tweet_times+","+tweettime, "TweetsTime")
-            print("user id %d tweettimes %d" % (userid, Twitter.GetStrAttrDatN(userid, "TweetsTime")))
-            threadLock.release()
-        else:
-            threadLock.release()
-            print("missing userid %d" % userid)
-        if start_point_id % 10000000 is 0:
-            print('saving the graph=========================')
-            threadLock.acquire()
-            fout = snap.TFOut("test-with-tweets.graph")
-            Twitter.Save(fout)
-            fout.Flush()
-            threadLock.release()
-            print('saved')
+        print("userid is %d and tweettime is %d" %(userid, tweettime))
+    # for [userid, tweettime] in cur:
+    #     threadLock.acquire()
+    #     if Twitter.IsNode(userid):
+    #         node_tweet_times = Twitter.GetStrAttrDatN(userid, "TweetsTime")
+    #         Twitter.AddStrAttrDatN(userid, node_tweet_times+","+tweettime, "TweetsTime")
+    #         print("user id %d tweettimes %d" % (userid, Twitter.GetStrAttrDatN(userid, "TweetsTime")))
+    #         threadLock.release()
+    #     else:
+    #         threadLock.release()
+    #         print("missing userid %d" % userid)
+    #     if start_point_id % 10000000 is 0:
+    #         print('saving the graph=========================')
+    #         threadLock.acquire()
+    #         fout = snap.TFOut("test-with-tweets.graph")
+    #         Twitter.Save(fout)
+    #         fout.Flush()
+    #         threadLock.release()
+    #         print('saved')
 
 
 class TweetsExtractor(threading.Thread):
@@ -57,7 +60,7 @@ start_points = Queue.Queue(500000)
 threadLock = threading.Lock()
 queueLock = threading.Lock()
 threads = []
-number_of_threads = 10
+number_of_threads = 1
 threadID = 0
 chunk_size = 10000
 
