544bb5d3a6d7429107f9693e255babda2614c10d
Erfan Tavakoli <erfan.tavakoli71@gmail.com>
2015-09-18 01:08:22 +0200

    exception handling added

--- FetchingTweetTimes.py
+++ FetchingTweetTimes.py
@@ -3,13 +3,13 @@ import threading
 import Queue
 from db_connector import DbConnection
 
-# FIn = snap.TFIn("test.graph")
-# Twitter = snap.TNEANet.Load(FIn)
-# """
-# :type Twitter:snap.TNEANet
-# """
-# print("the graph was loaded")
-# Twitter.AddStrAttrN("TweetsTime")
+FIn = snap.TFIn("test.graph")
+Twitter = snap.TNEANet.Load(FIn)
+"""
+:type Twitter:snap.TNEANet
+"""
+print("the graph was loaded")
+Twitter.AddStrAttrN("TweetsTime")
 
 
 def extract_tweets(thread_id, cur):
@@ -19,28 +19,27 @@ def extract_tweets(thread_id, cur):
 
     query = """select userid, tweettime from tweets where tweetid between %s and %s"""
     cur.execute(query, (start_point_id, start_point_id + chunk_size - 1))
-    cur.fetchall()
     print('thread %d has fetched data from %d to %d' % (thread_id, start_point_id, start_point_id + chunk_size -1))
     for [userid, tweettime] in cur:
-        print("userid is %d and tweettime is %d" %(userid, tweettime))
-    # for [userid, tweettime] in cur:
-    #     threadLock.acquire()
-    #     if Twitter.IsNode(userid):
-    #         node_tweet_times = Twitter.GetStrAttrDatN(userid, "TweetsTime")
-    #         Twitter.AddStrAttrDatN(userid, node_tweet_times+","+tweettime, "TweetsTime")
-    #         print("user id %d tweettimes %d" % (userid, Twitter.GetStrAttrDatN(userid, "TweetsTime")))
-    #         threadLock.release()
-    #     else:
-    #         threadLock.release()
-    #         print("missing userid %d" % userid)
-    #     if start_point_id % 10000000 is 0:
-    #         print('saving the graph=========================')
-    #         threadLock.acquire()
-    #         fout = snap.TFOut("test-with-tweets.graph")
-    #         Twitter.Save(fout)
-    #         fout.Flush()
-    #         threadLock.release()
-    #         print('saved')
+        print("userid is %d and tweettime is %d" %(userid, long(tweettime.strftime('%s'))))
+    for [userid, tweettime] in cur:
+        threadLock.acquire()
+        if Twitter.IsNode(userid):
+            node_tweet_times = Twitter.GetStrAttrDatN(userid, "TweetsTime")
+            Twitter.AddStrAttrDatN(userid, node_tweet_times+","+str(long(tweettime.strftime('%s'))), "TweetsTime")
+            print("user id %d tweettimes %d" % (userid, Twitter.GetStrAttrDatN(userid, "TweetsTime")))
+            threadLock.release()
+        else:
+            threadLock.release()
+            print("missing userid %d" % userid)
+        if start_point_id % 100000000 is 0:
+            print('saving the graph=========================')
+            threadLock.acquire()
+            fout = snap.TFOut("test-with-tweets.graph")
+            Twitter.Save(fout)
+            fout.Flush()
+            threadLock.release()
+            print('saved')
 
 
 class TweetsExtractor(threading.Thread):
@@ -54,14 +53,14 @@ class TweetsExtractor(threading.Thread):
             extract_tweets(self.thread_id, self.cur)
 
 conn = DbConnection()
-start_points = Queue.Queue(500000)
+start_points = Queue.Queue(50000)
 
 threadLock = threading.Lock()
 queueLock = threading.Lock()
 threads = []
-number_of_threads = 1
+number_of_threads = 10
 threadID = 0
-chunk_size = 10000
+chunk_size = 1000000
 
 start_point = 0
 queueLock.acquire()
@@ -73,17 +72,11 @@ queueLock.release()
 for i in range(number_of_threads):
     threadID += 1
     cursor = conn.get_cursor()
-    query = """select userid, tweettime from tweets where tweetid between %s and %s"""
-    cursor.execute(query, (0, 0 + chunk_size - 1))
-    cursor.fetchall()
-    for [userid, tweettime] in cursor:
-        print("userid is %d and tweettime is %d" %(userid, tweettime))
     thread = TweetsExtractor(threadID, cursor)
     threads.append(thread)
-    print("thread %d was created" % threadID)
-    # thread.start()
+    thread.start()
 
 
-# for t in threads:
-#     t.join()
+for t in threads:
+    t.join()
 
