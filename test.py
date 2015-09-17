efd35980a728eb7ed3fd7c3ef4582c3324c357dd
Erfan Tavakoli <erfan.tavakoli71@gmail.com>
2015-09-18 01:01:45 +0200

    exception handling added

--- test.py
+++ test.py
@@ -3,14 +3,11 @@ import ctypes
 import Queue
 from db_connector import DbConnection
 
-chunk_size = 10000
+chunk_size = 100000
 conn = DbConnection()
 cursor = conn.get_cursor()
 query = """select userid, tweettime from tweets where tweetid between %s and %s"""
 cursor.execute(query, (0, 0 + chunk_size - 1))
-cursor.fetchall()
-# for [userid, tweettime] in cursor:
-#     print("userid is %d and tweettime is %d" %(userid, tweettime))
 
-for row in cursor:
-    print(row)
+for [userid, tweettime] in cursor:
+    print("userid is %d and tweettime is %d" %(userid, tweettime))
