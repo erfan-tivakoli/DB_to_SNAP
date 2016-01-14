7fed18656906ff9f8ad77e5c33bf368abaef35e9
Erfan Tavakoli <erfan.tavakoli71@gmail.com>
2016-01-14 15:58:38 +0330

    the builder was added

---
+++ Data_Extractor/build_graph.py
@@ -0,0 +1,22 @@
+__author__ = 'Rfun'
+from FetchingLinks import GraphStructureBuilder
+from db_connector import DbConnection
+import snap
+
+
+def main():
+    conn = DbConnection
+    graph_builder = GraphStructureBuilder(conn)
+    graph_builder.fetch_add_links()
+    structured_graph = graph_builder.get_graph()
+
+    # saving the graph to a file
+    FOut = snap.TFOut("structured_graph.graph")
+    structured_graph.save(FOut)
+    FOut.Flush()
+
+    print 'structred graph was saved'
+
+
+if __name__ == '__main__':
+    main()
